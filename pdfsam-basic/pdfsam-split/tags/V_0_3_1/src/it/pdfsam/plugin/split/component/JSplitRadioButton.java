d31a7307a802546edf83ac3d89e04f03c6450fc6
torakiki <torakiki@2703fb93-1e26-0410-8fff-b03f68e33ab9>
2007-07-17 08:31:09 +0000

    Ver. 0.3.1

---
+++ pdfsam-basic/pdfsam-split/tags/V_0_3_1/src/it/pdfsam/plugin/split/component/JSplitRadioButton.java
@@ -0,0 +1,54 @@
+/*
+ * Created on 25-feb-2006
+ * Copyright (C) 2006 by Andrea Vacondio.
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the 
+ * GNU General Public License as published by the Free Software Foundation; 
+ * either version 2 of the License.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with this program; 
+ * if not, write to the Free Software Foundation, Inc., 
+ *  59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+package it.pdfsam.plugin.split.component;
+
+import javax.swing.JRadioButton;
+
+/**
+ * This component is used to get the split type selected by the user
+ * @author Andrea Vacondio
+ * @see it.pdfsam.console.MainConsole
+ *
+ */
+public class JSplitRadioButton extends JRadioButton {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 8679141401350692340L;
+    private String split_command;
+    
+    public JSplitRadioButton(String split_mode) {
+        super();
+        this.split_command = split_mode;
+        this.setActionCommand(split_mode);
+    }
+
+    /**
+     * @return Returns the split_command.
+     */
+    protected String getSplitCommand() {
+        return split_command;
+    }
+
+    /**
+     * @param split_command The split_command to set.
+     */
+    protected void setSplitCommand(String split_command) {
+        this.split_command = split_command;
+    }
+
+
+}
