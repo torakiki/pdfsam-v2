be4ebc8cb532374af6e3d99a802232c9af3abf44
torakiki <torakiki@2703fb93-1e26-0410-8fff-b03f68e33ab9>
2008-10-24 13:56:02 +0000

    Ver. 1.4.3e

---
+++ pdfsam-enhanced/pdfsam-maine/tags/V_1_4_3e/bin/run.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+### ====================================================================== ###
+##                                                                          ##
+##  pdfsam Bootstrap Script                                                  ##
+##                                                                          ##
+### ====================================================================== ###
+
+warn() {
+    echo "${PROGNAME}: $*"
+}
+
+die() {
+    warn $*
+    exit 1
+}
+
+
+DIRNAME="../"
+PDFSAMJAR="$DIRNAME/pdfsam-1.0.3.jar"
+
+# Setup the classpath
+if [ ! -f "$PDFSAMJAR" ]; then
+    die "Missing required file: $PDFSAMJAR"
+fi
+PDFSAM_CLASSPATH="$PDFSAMJAR"
+
+# Setup the JVM
+if [ "x$JAVA" = "x" ]; then
+    if [ "x$JAVA_HOME" != "x" ]; then
+	JAVA="$JAVA_HOME/bin/java"
+    else
+	JAVA="java"
+    fi
+fi
+
+# Setup pdfsam memory properties
+JAVA_OPTS="-Xmx256m"
+
+# Display our environment
+echo "========================================================================="
+echo ""
+echo " pdfsam"
+echo ""
+echo "  JAVA: $JAVA"
+echo ""
+echo "  JAVA_OPTS: $JAVA_OPTS"
+echo ""
+echo "  CLASSPATH: $PDFSAM_CLASSPATH"
+echo ""
+echo "========================================================================="
+echo ""
+
+# Execute the JVM in the foreground
+      "$JAVA" $JAVA_OPTS \
+         -classpath "$PDFSAM_CLASSPATH" \
+         org.pdfsam.guiclient.GuiClient "$@"
