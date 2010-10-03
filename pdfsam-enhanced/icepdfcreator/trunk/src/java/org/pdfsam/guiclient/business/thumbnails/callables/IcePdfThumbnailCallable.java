ea00365242cdbe6d4f732abfe5aef89bf651440e
torakiki <torakiki@2703fb93-1e26-0410-8fff-b03f68e33ab9>
2010-10-03 15:44:07 +0000

    Fixed bug #3072732 problem with ICEpdf render library and rotation.

--- pdfsam-enhanced/icepdfcreator/trunk/src/java/org/pdfsam/guiclient/business/thumbnails/callables/IcePdfThumbnailCallable.java
+++ pdfsam-enhanced/icepdfcreator/trunk/src/java/org/pdfsam/guiclient/business/thumbnails/callables/IcePdfThumbnailCallable.java
@@ -32,77 +32,79 @@ import org.pdfsam.guiclient.dto.Rotation;
 import org.pdfsam.guiclient.dto.VisualPageListItem;
 import org.pdfsam.guiclient.utils.ImageUtility;
 import org.pdfsam.i18n.GettextResource;
+
 /**
  * Callable used to generate thumbnails with ICEpdf
+ * 
  * @author Andrea Vacondio
- *
+ * 
  */
 public class IcePdfThumbnailCallable implements Callable<Boolean> {
-	
-	private static final Logger log = Logger.getLogger(IcePdfThumbnailCallable.class.getPackage().getName());
-	
-	private Document pdfDocument;
-	private JVisualPdfPageSelectionPanel panel;
-	private VisualPageListItem pageItem;
-	private long id;
-	
-	/**
-	 * @param pdfDocument document
-	 * @param panel
-	 * @param pageItem
-	 * @param id
-	 */
-	public IcePdfThumbnailCallable(Document pdfDocument, VisualPageListItem pageItem, JVisualPdfPageSelectionPanel panel, long id) {
-		super();
-		this.pdfDocument = pdfDocument;
-		this.panel = panel;
-		this.pageItem = pageItem;
-		this.id = id;
-	}
-	
-	public Boolean call() {
-		Boolean retVal = Boolean.FALSE;
-		if(!IdManager.getInstance().isCancelledExecution(id)){
-			try{
-				int pageNumber = pageItem.getPageNumber()-1;
-				PDimension dimensions = pdfDocument.getPageDimension(pageNumber, 0);
-				double rectHeight = dimensions.getHeight();
-				double recWidth = dimensions.getWidth();				
-				float  resizePercentage = (float)getResizePercentage(rectHeight, recWidth);
-				
-				BufferedImage scaledInstance = (BufferedImage) pdfDocument.getPageImage(pageNumber, GraphicsRenderingHints.SCREEN,Page.BOUNDARY_CROPBOX, 0, resizePercentage);				
-              	pageItem.setPaperFormat(recWidth, rectHeight, IcePdfThumbnailsCreator.ICEPDF_RESOLUTION);
-              	int rotation = (int)pdfDocument.getPageTree().getPage(pageNumber, this).getTotalRotation(0);
-              	if(rotation!=0){
-              		pageItem.setOriginalRotation(Rotation.getRotation(rotation));
-              	}
-              	if(pageItem.isRotated()){
-              		pageItem.setThumbnail(ImageUtility.rotateImage(scaledInstance, pageItem.getCompleteRotation()));	
-        		}else{
-        			pageItem.setThumbnail(scaledInstance);	
-        		}
-              	retVal = Boolean.TRUE;
-            }catch (Throwable t) {
-            	pageItem.setThumbnail(ImageUtility.getErrorImage());
-        		log.error(GettextResource.gettext(Configuration.getInstance().getI18nResourceBundle(),"Unable to generate thumbnail"),t);
-        	}
-            ((VisualListModel)panel.getThumbnailList().getModel()).elementChanged(pageItem);
-		}
+
+    private static final Logger log = Logger.getLogger(IcePdfThumbnailCallable.class.getPackage().getName());
+
+    private Document pdfDocument;
+    private JVisualPdfPageSelectionPanel panel;
+    private VisualPageListItem pageItem;
+    private long id;
+
+    /**
+     * @param pdfDocument
+     *            document
+     * @param panel
+     * @param pageItem
+     * @param id
+     */
+    public IcePdfThumbnailCallable(Document pdfDocument, VisualPageListItem pageItem,
+            JVisualPdfPageSelectionPanel panel, long id) {
+        super();
+        this.pdfDocument = pdfDocument;
+        this.panel = panel;
+        this.pageItem = pageItem;
+        this.id = id;
+    }
+
+    public Boolean call() {
+        Boolean retVal = Boolean.FALSE;
+        if (!IdManager.getInstance().isCancelledExecution(id)) {
+            try {
+                int pageNumber = pageItem.getPageNumber() - 1;
+                PDimension dimensions = pdfDocument.getPageDimension(pageNumber, 0);
+                double rectHeight = dimensions.getHeight();
+                double recWidth = dimensions.getWidth();
+                float resizePercentage = (float) getResizePercentage(rectHeight, recWidth);
+
+                BufferedImage scaledInstance = (BufferedImage) pdfDocument.getPageImage(pageNumber,
+                        GraphicsRenderingHints.SCREEN, Page.BOUNDARY_CROPBOX, 0, resizePercentage);
+                pageItem.setPaperFormat(recWidth, rectHeight, IcePdfThumbnailsCreator.ICEPDF_RESOLUTION);
+                int rotation = (int) pdfDocument.getPageTree().getPage(pageNumber, this).getTotalRotation(0);
+                if (rotation != 0) {
+                    pageItem.setOriginalRotation(Rotation.getRotation(rotation));
+                }
+                pageItem.setThumbnail(scaledInstance);
+                retVal = Boolean.TRUE;
+            } catch (Throwable t) {
+                pageItem.setThumbnail(ImageUtility.getErrorImage());
+                log.error(GettextResource.gettext(Configuration.getInstance().getI18nResourceBundle(),
+                        "Unable to generate thumbnail"), t);
+            }
+            ((VisualListModel) panel.getThumbnailList().getModel()).elementChanged(pageItem);
+        }
         return retVal;
-	}
-	
-	/**
-	 * @param height
-	 * @param width
-	 * @return percentage resize
-	 */
-	private double getResizePercentage(double height, double width){
-		double retVal = 0;
-		if(height>=width){
-			retVal = Math.round((ThumbnailsCreator.DEFAULT_SIZE/height)*100.0)/100.0;
-		}else{
-			retVal = Math.round((ThumbnailsCreator.DEFAULT_SIZE/width)*100.0)/100.0;
-		}
-		return retVal;
-	}
+    }
+
+    /**
+     * @param height
+     * @param width
+     * @return percentage resize
+     */
+    private double getResizePercentage(double height, double width) {
+        double retVal = 0;
+        if (height >= width) {
+            retVal = Math.round((ThumbnailsCreator.DEFAULT_SIZE / height) * 100.0) / 100.0;
+        } else {
+            retVal = Math.round((ThumbnailsCreator.DEFAULT_SIZE / width) * 100.0) / 100.0;
+        }
+        return retVal;
+    }
 }
