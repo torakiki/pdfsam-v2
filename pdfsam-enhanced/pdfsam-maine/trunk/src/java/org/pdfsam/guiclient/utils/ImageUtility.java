4d67af2469328d6709273ddb55b07ee3cc085b92
torakiki <torakiki@2703fb93-1e26-0410-8fff-b03f68e33ab9>
2012-08-23 16:55:53 +0000

    Increased quality of the scaled buffered image

--- pdfsam-enhanced/pdfsam-maine/trunk/src/java/org/pdfsam/guiclient/utils/ImageUtility.java
+++ pdfsam-enhanced/pdfsam-maine/trunk/src/java/org/pdfsam/guiclient/utils/ImageUtility.java
@@ -17,13 +17,19 @@ package org.pdfsam.guiclient.utils;
 import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.RenderingHints;
+import java.awt.RenderingHints.Key;
 import java.awt.image.BufferedImage;
+import java.awt.image.BufferedImageOp;
+import java.awt.image.ConvolveOp;
+import java.awt.image.Kernel;
 import java.awt.image.RenderedImage;
 import java.awt.image.renderable.ParameterBlock;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.imageio.ImageIO;
 import javax.media.jai.InterpolationNearest;
@@ -45,164 +51,167 @@ import org.pdfsam.i18n.GettextResource;
  */
 public class ImageUtility {
 
-	private static final Logger LOG = Logger.getLogger(ImageUtility.class.getPackage().getName());
+    private static final Logger LOG = Logger.getLogger(ImageUtility.class.getPackage().getName());
 
-	private static BufferedImage ERROR_IMAGE = null;
+    private static BufferedImage ERROR_IMAGE = null;
 
-	private static BufferedImage HOURGLASS = null;
+    private static BufferedImage HOURGLASS = null;
 
-	static {
-		System.setProperty("com.sun.media.jai.disableMediaLib", "true");
-	}
+    static {
+        System.setProperty("com.sun.media.jai.disableMediaLib", "true");
+    }
 
-	/**
-	 * @param inputImage
-	 * @param degrees
-	 * @return rotated image
-	 */
-	public static BufferedImage rotateImage(Image inputImage, int degrees) {
-		BufferedImage retVal = null;
-		RenderedImage ri = JAI.create("awtImage", inputImage);
-		ParameterBlock pb = new ParameterBlock();
-		pb.addSource(ri);
-		TransposeType rotOp = null;
-		RenderedOp op = null;
-		if (degrees == 90) {
-			rotOp = TransposeDescriptor.ROTATE_90;
-		} else if (degrees == 180) {
-			rotOp = TransposeDescriptor.ROTATE_180;
-		} else if (degrees == 270) {
-			rotOp = TransposeDescriptor.ROTATE_270;
-		}
-		if (rotOp != null) {
-			// use Transpose operation
-			pb.add(rotOp);
-			op = JAI.create("transpose", pb);
-		} else {
-			// setup "normal" rotation
-			pb.add(ri.getWidth() / 2.0f);
-			pb.add(ri.getHeight() / 2.0f);
-			pb.add((float) Math.toRadians(degrees));
-			pb.add(new InterpolationNearest());
-			op = JAI.create("Rotate", pb, null);
-		}
-		PlanarImage myPlanar = op.createInstance();
-		retVal = myPlanar.getAsBufferedImage();
-		return retVal;
-	}
+    /**
+     * @param inputImage
+     * @param degrees
+     * @return rotated image
+     */
+    public static BufferedImage rotateImage(Image inputImage, int degrees) {
+        BufferedImage retVal = null;
+        RenderedImage ri = JAI.create("awtImage", inputImage);
+        ParameterBlock pb = new ParameterBlock();
+        pb.addSource(ri);
+        TransposeType rotOp = null;
+        RenderedOp op = null;
+        if (degrees == 90) {
+            rotOp = TransposeDescriptor.ROTATE_90;
+        } else if (degrees == 180) {
+            rotOp = TransposeDescriptor.ROTATE_180;
+        } else if (degrees == 270) {
+            rotOp = TransposeDescriptor.ROTATE_270;
+        }
+        if (rotOp != null) {
+            // use Transpose operation
+            pb.add(rotOp);
+            op = JAI.create("transpose", pb);
+        } else {
+            // setup "normal" rotation
+            pb.add(ri.getWidth() / 2.0f);
+            pb.add(ri.getHeight() / 2.0f);
+            pb.add((float) Math.toRadians(degrees));
+            pb.add(new InterpolationNearest());
+            op = JAI.create("Rotate", pb, null);
+        }
+        PlanarImage myPlanar = op.createInstance();
+        retVal = myPlanar.getAsBufferedImage();
+        return retVal;
+    }
 
-	/**
-	 * @return an image displaying an error message
-	 */
-	public static BufferedImage getErrorImage() {
-		try {
-			if (ERROR_IMAGE == null) {
-				InputStream is = ImageUtility.class.getResourceAsStream("/images/thumbnailerror.png");
-				BufferedImage img = ImageIO.read(is);
-				is.close();
-				// convert to TYPE_INT_RGB
-				ERROR_IMAGE = new BufferedImage(img.getWidth(), img.getHeight(), BufferedImage.TYPE_INT_RGB);
-				Graphics2D g2d = (Graphics2D) ERROR_IMAGE.getGraphics();
-				g2d.drawImage(img, 0, 0, img.getWidth(), img.getHeight(), null);
-				g2d.dispose();
-			}
-		} catch (IOException e) {
-			LOG.error(GettextResource.gettext(Configuration.getInstance().getI18nResourceBundle(),
-					"Unable to create error image."));
-		}
-		return ERROR_IMAGE;
-	}
+    /**
+     * @return an image displaying an error message
+     */
+    public static BufferedImage getErrorImage() {
+        try {
+            if (ERROR_IMAGE == null) {
+                InputStream is = ImageUtility.class.getResourceAsStream("/images/thumbnailerror.png");
+                BufferedImage img = ImageIO.read(is);
+                is.close();
+                // convert to TYPE_INT_RGB
+                ERROR_IMAGE = new BufferedImage(img.getWidth(), img.getHeight(), BufferedImage.TYPE_INT_RGB);
+                Graphics2D g2d = (Graphics2D) ERROR_IMAGE.getGraphics();
+                g2d.drawImage(img, 0, 0, img.getWidth(), img.getHeight(), null);
+                g2d.dispose();
+            }
+        } catch (IOException e) {
+            LOG.error(GettextResource.gettext(Configuration.getInstance().getI18nResourceBundle(),
+                    "Unable to create error image."));
+        }
+        return ERROR_IMAGE;
+    }
 
-	/**
-	 * @return am image displaying an hourglass
-	 */
-	public static BufferedImage getHourglassImage() {
-		try {
-			if (HOURGLASS == null) {
-				InputStream is = ImageUtility.class.getResourceAsStream("/images/hourglass.png");
-				BufferedImage img = ImageIO.read(is);
-				is.close();
-				// convert to TYPE_INT_RGB
-				HOURGLASS = new BufferedImage(img.getWidth(), img.getHeight(), BufferedImage.TYPE_INT_RGB);
-				Graphics2D g2d = (Graphics2D) HOURGLASS.getGraphics();
-				g2d.drawImage(img, 0, 0, img.getWidth(), img.getHeight(), null);
-				g2d.dispose();
-			}
-		} catch (IOException e) {
-			LOG.error(GettextResource.gettext(Configuration.getInstance().getI18nResourceBundle(),
-					"Unable to create error image."));
-		}
-		return HOURGLASS;
-	}
+    /**
+     * @return am image displaying an hourglass
+     */
+    public static BufferedImage getHourglassImage() {
+        try {
+            if (HOURGLASS == null) {
+                InputStream is = ImageUtility.class.getResourceAsStream("/images/hourglass.png");
+                BufferedImage img = ImageIO.read(is);
+                is.close();
+                // convert to TYPE_INT_RGB
+                HOURGLASS = new BufferedImage(img.getWidth(), img.getHeight(), BufferedImage.TYPE_INT_RGB);
+                Graphics2D g2d = (Graphics2D) HOURGLASS.getGraphics();
+                g2d.drawImage(img, 0, 0, img.getWidth(), img.getHeight(), null);
+                g2d.dispose();
+            }
+        } catch (IOException e) {
+            LOG.error(GettextResource.gettext(Configuration.getInstance().getI18nResourceBundle(),
+                    "Unable to create error image."));
+        }
+        return HOURGLASS;
+    }
 
-	/**
-	 * @param o
-	 * @return a byte[] representing the input image
-	 * @throws IOException
-	 */
-	public static byte[] toByteArray(BufferedImage o) throws IOException {
-		if (o != null) {
-			BufferedImage image = o;
-			ByteArrayOutputStream baos = new ByteArrayOutputStream(1024);
-			ImageIO.write(image, "jpeg", baos);
-			byte[] b = baos.toByteArray();
-			return b;
-		}
-		return new byte[0];
-	}
+    /**
+     * @param o
+     * @return a byte[] representing the input image
+     * @throws IOException
+     */
+    public static byte[] toByteArray(BufferedImage o) throws IOException {
+        if (o != null) {
+            BufferedImage image = o;
+            ByteArrayOutputStream baos = new ByteArrayOutputStream(1024);
+            ImageIO.write(image, "jpeg", baos);
+            byte[] b = baos.toByteArray();
+            return b;
+        }
+        return new byte[0];
+    }
 
-	/**
-	 * @param imagebytes
-	 * @return the Buffered image represented by the byte[]
-	 * @throws IOException
-	 */
-	public static BufferedImage fromByteArray(byte[] imagebytes) throws IOException {
-		BufferedImage retVal = null;
-		if (imagebytes != null && (imagebytes.length > 0)) {
-			retVal = ImageIO.read(new ByteArrayInputStream(imagebytes));
-		}
-		return retVal;
-	}
+    /**
+     * @param imagebytes
+     * @return the Buffered image represented by the byte[]
+     * @throws IOException
+     */
+    public static BufferedImage fromByteArray(byte[] imagebytes) throws IOException {
+        BufferedImage retVal = null;
+        if (imagebytes != null && (imagebytes.length > 0)) {
+            retVal = ImageIO.read(new ByteArrayInputStream(imagebytes));
+        }
+        return retVal;
+    }
 
-	/**
-	 * 
-	 * @param img
-	 *            input image
-	 * @param targetWidth
-	 * @param targetHeight
-	 * @return a scaled image
-	 */
-	public static BufferedImage getScaledInstance(BufferedImage img, int targetWidth, int targetHeight) {
-		int type = (img.getTransparency() == java.awt.Transparency.OPAQUE) ? BufferedImage.TYPE_INT_RGB
-				: BufferedImage.TYPE_INT_ARGB;
-		BufferedImage ret = (BufferedImage) img;
-		int w = img.getWidth();
-		int h = img.getHeight();
+    /**
+     * 
+     * @param img
+     *            input image
+     * @param targetWidth
+     * @param targetHeight
+     * @return a scaled image
+     */
+    public static BufferedImage getScaledInstance(BufferedImage img, int targetWidth, int targetHeight) {
+        int type = (img.getTransparency() == java.awt.Transparency.OPAQUE) ? BufferedImage.TYPE_INT_RGB
+                : BufferedImage.TYPE_INT_ARGB;
+        BufferedImage ret = (BufferedImage) img;
+        int w = img.getWidth();
+        int h = img.getHeight();
 
-		do {
-			if (w > targetWidth) {
-				w /= 2;
-				if (w < targetWidth) {
-					w = targetWidth;
-				}
-			}
+        do {
+            if (w > targetWidth) {
+                w /= 2;
+                if (w < targetWidth) {
+                    w = targetWidth;
+                }
+            }
 
-			if (h > targetHeight) {
-				h /= 2;
-				if (h < targetHeight) {
-					h = targetHeight;
-				}
-			}
+            if (h > targetHeight) {
+                h /= 2;
+                if (h < targetHeight) {
+                    h = targetHeight;
+                }
+            }
 
-			BufferedImage tmp = new BufferedImage(w, h, type);
-			Graphics2D g2 = tmp.createGraphics();
-			g2.setRenderingHint(java.awt.RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
-			g2.drawImage(ret, 0, 0, w, h, null);
-			g2.dispose();
+            BufferedImage tmp = new BufferedImage(w, h, type);
+            Graphics2D g2 = tmp.createGraphics();
+            g2.setRenderingHint(java.awt.RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+            g2.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
 
-			ret = tmp;
-		} while (w != targetWidth || h != targetHeight);
+            g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+            g2.drawImage(ret, 0, 0, w, h, null);
+            g2.dispose();
 
-		return ret;
-	}
+            ret = tmp;
+        } while (w != targetWidth || h != targetHeight);
+
+        return ret;
+    }
 }
